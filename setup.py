a062f7a9b392cb301a33ee02d084b58dfa372b34
Andre Kupka <kupka@in.tum.de>
2016-02-17 01:43:20 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+
+setup(
+    name="pwny",
+    version="0.1.2",
+    author="André Kupka",
+    author_email="kupka@in.tum.de",
+    description="A small binary exploitation framework.",
+    license="MIT",
+    keywords="binary exploitation pwning pwny",
+    url="https://github.com/fr3akout/pwny",
+    packages=["pwny"],
+    long_description="A small binary exploitation framework, that provides "
+            "functionality to communicate with remote servers and write rop "
+            "chains.",
+    classifiers=[]
+)
